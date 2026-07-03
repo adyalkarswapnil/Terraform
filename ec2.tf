f3a152b7e40986f3bb05ee0179c0579002e4718c
Swapnil Adyalkar <adyalkarswapnil@gmail.com>
2026-07-04 05:16:17 +0530

    FC

---
+++ ec2.tf
@@ -0,0 +1,69 @@
+#key pair(login)
+resource "aws_key_pair" "My_key" {
+  key_name   = "terra-key-ec2"
+  public_key = file("terra-key-ec2.pub")
+}
+#VPC & Security Group
+resource "aws_default_vpc" "default" {
+  
+}
+
+resource "aws_security_group" "my_security_group" {
+    name = "automate-sg"
+    description = "this will add a TF generated security group"
+    vpc_id = aws_default_vpc.default.id  #interpolation
+
+     
+ #inbound rules
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "SSH open"
+    }    
+
+    ingress  {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "HTTP open"
+    }
+    
+    ingress  {
+        from_port = 8000
+        to_port = 8000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "Notes app"
+    }
+     #outbound rules
+    
+    egress  {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks =["0.0.0.0/0"]
+        description ="all access open outbound"
+    }
+
+    tags = {
+        Name = automate-sg
+    }
+}
+# ec2 Instance
+resource "aws_instance" "my_instance" {
+    key_name = aws_key_pair.My_key.key_name
+    security_groups = aws_security_group.my_security_group.automate-sg
+    instance_type = "t2.micro"
+    ami = "ami-01a00762f46d584a1" #ubuntu
+
+    root_block_device {
+        volume_size = 15
+        volume_type = "gp3"
+    }
+    tags = {
+       Name = "TWS-Junoon-automate"
+    }
+}
