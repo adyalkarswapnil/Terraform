0e19f78bf59db0899cf8175217a23b55649815d7
Swapnil Adyalkar <adyalkarswapnil@gmail.com>
2026-07-09 01:55:08 +0530

    first comment

---
+++ ec2.tf
@@ -0,0 +1,74 @@
+# Key Pair
+resource "aws_key_pair" "My_key" {
+  key_name   = "terra-key-ec2"
+  public_key = file("terra-key-ec2.pub")
+}
+
+# Default VPC
+resource "aws_default_vpc" "default" {}
+
+# Security Group
+resource "aws_security_group" "my_security_group" {
+  name        = "automate-sg"
+  description = "Terraform Security Group"
+  vpc_id      = aws_default_vpc.default.id
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH"
+  }
+
+  # HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP"
+  }
+
+  # Notes App
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Notes App"
+  }
+
+  # Outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "automate-sg"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "my_instance" {
+  ami                    = var.ec2_ami_id
+  instance_type          = var.ec2_instance_type
+  key_name               = aws_key_pair.My_key.key_name
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+
+  user_data = file("${path.module}/install_nginx.sh")
+
+  root_block_device {
+    volume_size = var.ec2_root_storage_size
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "TWS-Junoon-automate"
+  }
+}
