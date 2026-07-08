7245fef7e80b37d3a15392830f50fbf601f98120
Swapnil Adyalkar <adyalkarswapnil@gmail.com>
2026-07-09 00:39:12 +0530

    first comment

--- ec2.tf
+++ ec2.tf
@@ -1,67 +1,70 @@
-#key pair(login)
+# Key Pair
 resource "aws_key_pair" "My_key" {
   key_name   = "terra-key-ec2"
-  public_key = "${file("${path.module}/terra-key-ec2.pub") }"
-}
-#VPC & Security Group
-resource "aws_default_vpc" "default" {
+  public_key = file("${path.module}/terra-key-ec2.pub")
 }
 
+# Default VPC
+resource "aws_default_vpc" "default" {}
+
+# Security Group
 resource "aws_security_group" "my_security_group" {
-    name = "automate-sg"
-    description = "this will add a TF generated security group"
-    vpc_id = "${aws_default_vpc.default.id}"               #interpolation
+  name        = "automate-sg"
+  description = "Terraform Security Group"
+  vpc_id      = aws_default_vpc.default.id
 
-     
- #inbound rules
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        description = "SSH open"
-    }    
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH"
+  }
 
-    ingress  {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        description = "HTTP open"
-    }
-    
-    ingress  {
-        from_port = 8000
-        to_port = 8000
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        description = "Notes app"
-    }
-     #outbound rules
-    
-    egress  {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks =["0.0.0.0/0"]
-        description ="all access open outbound"
-    }
+  # HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP"
+  }
 
-    tags = {
-        Name = "automate-sg"
-    }
+  # Notes App
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Notes App"
+  }
+
+  # Outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "automate-sg"
+  }
 }
-# ec2 Instance
+
+# EC2 Instance
 resource "aws_instance" "my_instance" {
-  ami                    = "${var.ec2_ami_id}"
-  instance_type          = "${var.ec2_instance_type}"
-  key_name               = "${aws_key_pair.My_key.key_name}"
-  vpc_security_group_ids = [
-    "${aws_security_group.my_security_group.id}"
-  ]
-  user_data = "${file("${path.module}/install_nginx.sh") }"
+  ami                    = var.ec2_ami_id
+  instance_type          = var.ec2_instance_type
+  key_name               = aws_key_pair.My_key.key_name
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+
+  user_data = file("${path.module}/install_nginx.sh")
+
   root_block_device {
-    volume_size = "${var.ec2_root_storage_size}"
+    volume_size = var.ec2_root_storage_size
     volume_type = "gp3"
   }
 
