bdf7898a396cfa8176003931a848bf616a7780b9
Swapnil Adyalkar <adyalkarswapnil@gmail.com>
2026-07-12 16:31:45 +0530

    first comment

--- variable.tf
+++ variable.tf
@@ -1,6 +1,7 @@
 variable "ec2_instance_type" {
-    type = string
-    default = "t3.small"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.small"
 }
 
 variable "ec2_root_storage_size"{
