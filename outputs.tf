49f0b68a55e480a2076f141826cad0a1ca68b3d9
Swapnil Adyalkar <adyalkarswapnil@gmail.com>
2026-07-15 10:05:58 +0530

    first comment

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@
 #output "ec2_private_ip" {
  # value = aws_instance.my_instance[*].private_ip          #[*] stand for multiple instance create
  # }
+
+output "ec2_public_ip" {
+  value = [ for instance in aws_instance.my_instance : instance.public_ip ]
+}
