c930e4a3099b48c0cca2231b2263cb1942a52ff0
Swapnil Adyalkar <adyalkarswapnil@gmail.com>
2026-07-17 01:12:53 +0530

    first comment

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,5 @@
+# Outputs for count
+
 #output "ec2_public_ip" {
  # value = aws_instance.my_instance[*].public_ip   #[*] stand for multiple instance create & without [*] single instance  }
 #}
@@ -10,6 +12,16 @@
  # value = aws_instance.my_instance[*].private_ip          #[*] stand for multiple instance create
  # }
 
+
+#outputs for For_Each
 output "ec2_public_ip" {
   value = [ for instance in aws_instance.my_instance : instance.public_ip ]
+}
+
+output "ec2_public_dns" {
+  value = [ for instance in aws_instance.my_instance : instance.public_dns ]
+}
+
+output "ec2_private_ip" {
+  value = [ for instance in aws_instance.my_instance : instance.private_ip ]
 }
