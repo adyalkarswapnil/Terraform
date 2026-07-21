cf704400f20922a6335ac9ef2c10b9fae84d29eb
Swapnil Adyalkar <swapnilswapnil20@gmail.com>
2026-07-22 02:44:21 +0530

    first commit

--- terraform.tf
+++ terraform.tf
@@ -5,11 +5,12 @@ terraform {
       version = "6.53.0"
     }
   }
-}
+
 
 backend "s3" {
   bucket = "deviji-junoon-state-buckets"
   key    = "terraform.tfstate"
   region = "us-east-2"
   dynamodb_table = "TWS-Junoon_state-table"
+}
 }
